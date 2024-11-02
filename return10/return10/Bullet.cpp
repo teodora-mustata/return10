a118a04b61d101f336e288e3d66b738740d6e363
Sebi23647 <radeasebastian@gmail.com>
2024-11-02 18:49:32 +0200

    Added functionality for check collision

--- return10/return10/Bullet.cpp
+++ return10/return10/Bullet.cpp
@@ -5,6 +5,40 @@ Bullet::Bullet(int startX, int startY, Direction dir, float speed)
 {
 }
 
+
+bool Bullet::checkPlayerCollision(Player& target) {
+     int targetX = target.GetPosition().first;
+     int targetY = target.GetPosition().second;
+
+     if (x == targetX && y == targetY) {
+        target.resetPosition(); 
+        deactivate(); 
+        target.loseLife();
+        return true; 
+     }
+     return false; 
+}
+
+
+bool Bullet::checkWallCollision(Map& map) {
+    CellType cell = map.get_cell_type(x, y);
+
+        
+    if (auto* wall = std::get_if<Wall>(&cell)) {
+        if (wall->is_destructible()) {
+                map.set_cell_type(x, y, std::monostate{}); 
+                deactivate(); 
+                return true; 
+        }
+        else {
+            deactivate(); 
+            return true; 
+         }
+    }
+    return false; 
+}
+
+
 void Bullet::move() {
     auto offset = DirectionUtils::toOffset(direction);
     x += static_cast<int>(offset.first * speed);
--- return10/return10/Bullet.h
+++ return10/return10/Bullet.h
@@ -3,19 +3,24 @@
 #include <chrono>
 #include "DirectionUtils.h"
 #include "Map.h"
+#include "Player.h"
 class Bullet
 {
 public:
     int x; 
     int y; 
     Direction direction; 
-    float speed; 
+    float speed=0.25; 
     bool active; 
     std::chrono::time_point<std::chrono::steady_clock> fired_at; 
 
     Bullet(int startX, int startY, Direction dir, float speed);
 
     void move();
+    bool checkPlayerCollision(Player& target);
+    bool checkWallCollision(Map& map);
     void deactivate();
+    //todo modify bullet speed
+    //if speed==0.25 
 };
 
