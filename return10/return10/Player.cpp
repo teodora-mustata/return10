cf922ba575ed6ff12bc31f9f8aec341cb76810a6
PelesStefania <stefania.peles@student.untibv.ro>
2024-11-02 16:14:06 +0200

    Added Getters and updates for player

--- return10/return10/Player.cpp
+++ return10/return10/Player.cpp
@@ -4,14 +4,15 @@ Player::Player(std::string name, int startX, int startY):
 	m_name(name),
 	m_score(0),
 	m_lives(3),
-	m_points(0)
+	m_points(0),
+	m_initial_position(startX, startY)
 {
-	position = new std::pair<int, int>(startX, startY);
+	m_position = new std::pair<int, int>(startX, startY);
 }
 
 Player::~Player()
 {
-	delete position;
+	delete m_position;
 }
 
 void Player::move(Direction direction)
@@ -19,16 +20,55 @@ void Player::move(Direction direction)
 	switch (direction)
 	{
 	case Direction::UP:
-		position->second += 1;
+		m_position->second += 1;
 		break;
 	case Direction::DOWN:
-		position->second -= 1;
+		m_position->second -= 1;
 		break;
 	case Direction::LEFT:
-		position->first -= 1;
+		m_position->first -= 1;
 		break;
 	case Direction::RIGHT:
-		position->first += 1;
+		m_position->first += 1;
 		break;
+	}
+}
+
+std::string Player::GetName() const
+{
+	return m_name;
+}
+
+int Player::GetScore() const
+{
+	return m_score;
+}
+
+int Player::GetLives() const
+{
+	return m_lives;
+}
+
+std::pair<int, int> Player::GetPosition() const
+{
+	return *m_position;
+}
+
+void Player::addPoints(int acumulated_points)
+{
+	m_points += acumulated_points;
+}
+
+void Player::resetPosition()
+{
+	*m_position = m_initial_position;
+}
+
+void Player::loseLife()
+{
+	if (m_lives > 0)
+	{
+		m_lives -= 1;
+		resetPosition();
 	}
 }
--- return10/return10/Player.h
+++ return10/return10/Player.h
@@ -8,13 +8,23 @@ public:
 	~Player();
 	void move(Direction direction);
 
+	//Getters
+	std::string GetName() const;
+	int GetScore() const;
+	int GetLives() const;
+	std::pair<int, int> GetPosition() const;
+
+	//Updates for player
+	void addPoints(int acumulated_points);
+	void resetPosition();
+	void loseLife();
 private:
 	int m_score;
 	int m_lives;
 	int m_points;
 	std::string m_name;
-	std::pair<int, int>* position;
-
+	std::pair<int, int>* m_position;
+	std::pair<int, int> m_initial_position;
 
 };
 
