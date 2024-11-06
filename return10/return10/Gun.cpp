6b52621d0b0305ea2ded233789e61d8995854954
mocanuEmanuel <mocanuemanuel19@proton.me>
2024-11-06 17:00:21 +0200

    Added updateBullets method to remove bullets from m_firedBullets once collided

--- return10/return10/Gun.cpp
+++ return10/return10/Gun.cpp
@@ -1,28 +1,49 @@
 #include "Gun.h"
 
-Gun::Gun() : lastFiredTime(std::chrono::steady_clock::now()) {}
+Gun::Gun() : m_lastFiredTime(std::chrono::steady_clock::now()) {}
 
 bool Gun::fire(int playerX, int playerY, Direction playerDirection, float bulletSpeed)
 {
     auto now = std::chrono::steady_clock::now();
-    std::chrono::duration<float> timeSinceLastShot = now - lastFiredTime;
+    std::chrono::duration<float> timeSinceLastShot = now - m_lastFiredTime;
 
-    if (timeSinceLastShot.count() >= firingRate.count())
+    if (timeSinceLastShot.count() >= m_firingRate.count())
     {
         Bullet newBullet(playerX, playerY, playerDirection, bulletSpeed);
         newBullet.active = true;
         newBullet.fired_at = now;
 
-        firedBullets.push_back(newBullet);
-        lastFiredTime = now;
+        m_firedBullets.push_back(newBullet);
+        m_lastFiredTime = now;
 
         return true; //Fired
     }
     return false; // Not enough time has passed to fire again
 }
 
+//checks to see status of bullets and removes any inactive bullets that have collided from the m_firedBullets vector
+//method called 
+void Gun::updateBullets(Map& map, Player& target)
+{   //needs to stay with iterator not range base because by removing from the vector it messes up the range base type for
+    //need to choose where to call this and where if continous or maybe change it to once we see bullet collided somewhere else sent the message to 
+    //simply erase not sure yet
+    for (auto currentBullet = m_firedBullets.begin(); currentBullet != m_firedBullets.end(); ) {
+        currentBullet->checkPlayerCollision(target);
+        currentBullet->checkWallCollision(map);
+
+        if (!currentBullet->active) {
+            // if erased put iterator one place back because erase sets it further
+            currentBullet = m_firedBullets.erase(currentBullet);
+        }
+        else {
+            ++currentBullet; // only increment if we didnt erase
+        }
+    }
+
+}
+
 std::vector<Bullet> Gun::getBullets() const //I deleted const ref because I need to acces the last bullet in player class
 {
-    return firedBullets; 
+    return m_firedBullets; 
 }
 
--- return10/return10/Gun.h
+++ return10/return10/Gun.h
@@ -3,16 +3,20 @@
 #include "Direction.h"
 #include <chrono>
 #include <vector>
+#include <algorithm>
+#include "Bullet.h"
+#include "Map.h"
 
 class Gun
 {
 private:
-    std::chrono::duration<float> firingRate = std::chrono::seconds(4);
-    std::chrono::time_point<std::chrono::steady_clock> lastFiredTime;
-    std::vector<Bullet> firedBullets;
+    std::chrono::duration<float> m_firingRate = std::chrono::seconds(4);
+    std::chrono::time_point<std::chrono::steady_clock> m_lastFiredTime;
+    std::vector<Bullet> m_firedBullets;
 
 public:
     Gun();
     bool fire(int playerX, int playerY, Direction playerDirection, float bulletSpeed);
+    void updateBullets(Map& map, Player& target);
     std::vector<Bullet> getBullets() const;
 };
