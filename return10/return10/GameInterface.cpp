dc9aefa17ba8146e837e4b23c75795f0d02a073c
mocanuEmanuel <mocanuemanuel19@proton.me>
2024-11-07 19:53:10 +0200

    Implemented the base of a GameInterface class to use in the terminal test

---
+++ return10/return10/GameInterface.cpp
@@ -0,0 +1,64 @@
+#include "GameInterface.h"
+
+
+GameInterface::GameInterface(int mapWidth, int mapHeight, int numPlayers)
+    : m_gameMap(mapWidth, mapHeight) {
+    initializePlayers(numPlayers);
+    m_gameMap.generateWalls();
+    m_gameMap.generateSpawnPoints();
+}
+void GameInterface::mainLoop() {
+    m_gameMap.printMap(); //more to do here
+}
+void GameInterface::startGame() {
+    std::cout << "Starting login process..." << std::endl;
+    login(); 
+    std::cout << "Game started with " << m_players.size() << " players!" << std::endl;
+    mainLoop();
+}
+void GameInterface::displayStatus() {
+    for (const auto& player : m_players) {
+        std::cout << player.GetName() << ": Lives=" << player.GetLives()
+            << ", Score=" << player.GetScore()
+            << ", Position=(" << player.GetPosition().first
+            << ", " << player.GetPosition().second << ")\n";
+    }
+}
+
+void GameInterface::login() {
+    std::string username;
+    bool loginSuccessful = false;
+
+    while (!loginSuccessful) {
+        std::cout << "Enter a username to log in: ";
+        std::cin >> username;
+        loginSuccessful = m_loginManager.loginUser(username);
+    }
+}
+
+    void GameInterface::handlePlayerMove(Player & player) {
+        std::string dir;
+        std::cout << "Enter direction (up, down, left, right): ";
+        std::cin >> dir;
+
+        Direction direction; //need to get the direction from the directionUtils here
+        player.move(direction);
+    }
+
+    void GameInterface::handlePlayerShoot(Player & player) {
+        std::string dir;
+        std::cout << "Enter shooting direction (up, down, left, right): ";
+        std::cin >> dir;
+
+        Direction direction; //need to get the direction from the directionUtils here
+        player.shoot(direction, 0.25);  
+    }
+
+    
+
+
+
+
+
+
+
---
+++ return10/return10/GameInterface.h
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include "Map.h"
+#include "Player.h"
+#include "Direction.h"
+#include "LoginManager.h"
+
+class GameInterface {
+private:
+    Map m_gameMap;
+    std::vector<std::unique_ptr<Player>> m_players;
+    LoginManager m_loginManager; 
+
+
+public:
+    GameInterface(int mapWidth, int mapHeight, int numPlayers);
+    void startGame();
+    void initializePlayers(int numPlayers);
+    void mainLoop();
+    void handlePlayerMove(Player& player);
+    void handlePlayerShoot(Player& player);
+    void displayStatus();
+    void checkWallCollision(Bullet& bullet);
+
+
+    void login();
+};
